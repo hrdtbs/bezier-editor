187087befc4159f5b054c6944d5efc430276e7cf
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-09-16 22:55:36 +0900

    chore(stories): add new examples

--- src/BezierEditor.stories.tsx
+++ src/BezierEditor.stories.tsx
@@ -13,3 +13,25 @@ export default {
 const Template: Story<BezierEditorProps> = (args) => <BezierEditor {...args} />
 
 export const BasicUsage = Template.bind({})
+
+export const WithLabels = Template.bind({})
+
+WithLabels.args = {
+    xAxisLabel: "Time Percentage",
+    yAxisLabel: "Progress Percentage",
+}
+
+export const ReadOnly = Template.bind({})
+
+ReadOnly.args = {
+    readOnly: true,
+    defaultValue: [0.8, 0.2, 0.2, 0.8],
+}
+
+export const WithProgress = Template.bind({})
+
+WithProgress.args = {
+    progress: 0.42,
+    progressColor: "coral",
+    defaultValue: [0.8, 0.2, 0.2, 0.8],
+}
