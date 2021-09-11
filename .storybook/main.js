780db8962c0938e9e4b1f7e0cafef9afeaae39f3
hrdtbs <harada.tsubasa.0422@gmail.com>
2021-09-11 18:16:29 +0900

    chore(storybook): update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const babel = require("@babel/core");
-
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -8,17 +6,5 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ],
-  babel: (config) => {
-    const babelConfig = babel.loadPartialConfig({
-      configFile: require.resolve('../.babelrc.js'),
-    });
-    if (!babelConfig) {
-      throw new Error('Failed to load Babel config');
-    }
-    return {
-      ...config,
-      ...babelConfig.options,
-    };
-  },
+  ]
 }
